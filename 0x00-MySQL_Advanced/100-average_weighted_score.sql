6ae942fca92e650eca9c6999a25fde86f2f17862
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-22 00:27:52 +0000

    100-average_weighted_score.sql

---
+++ 0x00-MySQL_Advanced/100-average_weighted_score.sql
@@ -0,0 +1,18 @@
+-- THE SQL SCRIOT CREATES A STORED PROCEDURE
+-- THAT COMPUTES AVERAGE SCORE.
+DROP PROCEDURE IF EXISTS ComputeAverageWeightedScoreForUser;
+DELIMITER $$
+CREATE PROCEDURE ComputeAverageWeightedScoreForUser(
+    user_id INT
+)
+BEGIN
+    DECLARE avg_cal_score FLOAT;
+    SET avg_cal_score = (SELECT SUM(score * weight) / SUM(weight) 
+                        FROM users AS U 
+                        JOIN corrections as C ON U.id=C.user_id 
+                        JOIN projects AS P ON C.project_id=P.id 
+                        WHERE U.id=user_id);
+    UPDATE users SET average_score = avg_cal_score WHERE id=user_id;
+END
+$$
+DELIMITER ;
