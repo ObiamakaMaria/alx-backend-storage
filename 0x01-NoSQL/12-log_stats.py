bc897cf3104024a78aa40eea4c883b5457792e22
ObiamakaMaria <obiamaka0101@gmail.com>
2024-03-26 16:53:07 -0700

    12-log_stats.py

---
+++ 0x01-NoSQL/12-log_stats.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+    This script displays stats about nginx logs stored in mongoDb
+"""
+import pymongo
+
+
+if __name__ == '__main__':
+    server = pymongo.MongoClient('mongodb://127.0.0.1:27017/')
+
+    x = server.logs.nginx
+    lis = x.count_documents({})
+    print(f"{lis} logs")
+
+    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    print("Methods:")
+
+    for method in methods:
+        num_of_lis = x.count_documents({"method": methods})
+        print(f"\t method {method}: {num_of_lis}")
+
+    status_count = x.count_documents({"method": "GET", "path": "/status"})
+    print(f"{status_count} status check")
